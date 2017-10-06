8da8a3b87f5c3b4bfc1192323d7f5b655c1c52fa
[bmendezf] <[brunomendez93@gmail.com]>
2017-10-06 10:45:53 -0300

    Obligatorio vacío

---
+++ Obligatorio/ERP.Test/Teacher/TeacherTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ERP.Entities.Teacher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Entities.Teacher.Tests
+{
+    [TestClass()]
+    public class TeacherTests
+    {
+        [TestMethod()]
+        public void mTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
