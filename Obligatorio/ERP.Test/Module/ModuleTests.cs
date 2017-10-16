2be86a443f886e7e8bdfcabd444a379c3be9e235
Paola Francescoli <paolafrancescoli@gmail.com>
2017-10-16 12:54:07 -0700

    crear pruebas de modulo

---
+++ Obligatorio/ERP.Test/Module/ModuleTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ERP.Entities.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Entities.Module.Tests
+{
+    [TestClass()]
+    public class ModuleTests
+    {
+        [TestMethod()]
+        public void nameNotNullTest()
+        {
+            Module aModule = new Module();
+
+            Assert.IsNotNull(aModule.Name);
+        }
+
+        [TestMethod()]
+        public void descriptionNotNullTest()
+        {
+            Module aModule = new Module();
+
+            Assert.IsNotNull(aModule.Description);
+        }
+        [TestMethod()]
+        public void actionsNotNullTest()
+            Module aModule = new Module();
+
+            Assert.IsNotNull(aModule.Actions);
+        }
+
+    }
+}
