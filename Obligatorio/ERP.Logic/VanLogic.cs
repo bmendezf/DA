1dcbf8a087a79c984d699e3b783945242d97b014
DESKTOP-T31QH76\bruno <brunomendez93@gmail.com>
2017-10-16 19:26:45 -0300

    addVanTest pasa

--- Obligatorio/ERP.Logic/VanLogic.cs
+++ Obligatorio/ERP.Logic/VanLogic.cs
@@ -8,5 +8,27 @@ namespace ERP.Logic
 {
     public class VanLogic
     {
+        public static bool addVan(Entities.Van.Van aVan)
+        {
+            if (ERP.Data.Data.vans.Contains(aVan))
+            {
+                return false;
+            }
+            ERP.Data.Data.vans.Add(aVan);
+            return true;
+        }
+
+        public static Entities.Van.Van getVan(Entities.Van.Van aVan)
+        {
+            foreach (Entities.Van.Van van in ERP.Data.Data.vans)
+            {
+                if (van.Equals(aVan))
+                {
+                    return van;
+                }
+            }
+            return aVan;
+        }
+
     }
 }
