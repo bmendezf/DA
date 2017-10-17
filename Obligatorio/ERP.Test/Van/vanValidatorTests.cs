0ef7f2ad27fd9ea40a50f789d173d691b8c08902
[bmendezf] <[brunomendez93@gmail.com]>
2017-10-17 16:38:22 -0300

    vanValidator tests

---
+++ Obligatorio/ERP.Test/Van/vanValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ERP.Test.Van
+{
+    [TestClass]
+    public class vanValidatorTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            [TestMethod()]
+        public void correctLicencePlateTest()
+        {
+            Assert.IsTrue(ERP.Entities.Validators.Student.StudentValidator.CIValidator("abc1234"));
+        }
+
+        [TestMethod()]
+        public void incorrectLicencePlateTest()
+        {
+            Assert.IsFalse(ERP.Entities.Validators.Student.StudentValidator.CIValidator("654"));
+        }
+    }
+    }
+}
