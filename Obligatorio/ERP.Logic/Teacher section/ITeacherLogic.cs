62981ad1866dde72487adc4c243709f034301e66
Paola Francescoli <paolafrancescoli@gmail.com>
2017-10-17 04:06:09 -0700

    firmas de metodos de modulo docente

---
+++ Obligatorio/ERP.Logic/Teacher section/ITeacherLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Entities.Teacher;
+
+
+namespace ERP.Logic
+{
+    interface ITeacherLogic
+    {
+        bool AddTeacher(Teacher aTeacher);
+
+        bool DeleteTeacher(Teacher aTeacher);
+
+        bool UpdateTeacher(Teacher aTeacher);
+
+        List<Teacher> GetAllTeachers();
+    }
+}
---
+++ Obligatorio/ERP.Logic/Teacher section/TeacherLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Entities.Teacher;
+
+
+namespace ERP.Logic
+{
+    public class TeacherLogic: ITeacherLogic
+    {
+        private Data.Data database;
+
+        public TeacherLogic(Data.Data someDatabase)
+        {
+            this.database = someDatabase;
+        }
+        public bool AddTeacher(Teacher aTeacher) { return false; }
+
+        public bool DeleteTeacher(Teacher aTeacher) { return false; }
+
+        public bool UpdateTeacher(Teacher aTeacher) { return false; }
+
+        public List<Teacher> GetAllTeachers() { return null; }
+    }
+}
