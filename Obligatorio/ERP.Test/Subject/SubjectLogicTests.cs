1c4d95d8face95f4c71da9cd641b1725e889ef43
Paola Francescoli <paolafrancescoli@gmail.com>
2017-10-17 03:46:01 -0700

    se agregan tests de modulo materia

--- Obligatorio/ERP.Test/Subject/SubjectLogicTests.cs
+++ Obligatorio/ERP.Test/Subject/SubjectLogicTests.cs
@@ -5,39 +5,155 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERP.Data;
+using ERP.Entities.Subject;
+using ERP.Entities.Student;
+using ERP.Entities.Teacher;
 
 namespace ERP.Logic.Tests
 {
     [TestClass()]
     public class SubjectLogicTests
     {
+        Data.Data aDatabase = new Data.Data();
+        
+
+        [TestMethod()]
+        public void AddSubjectTrueTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            Assert.IsTrue(sub.AddSubject(aSubject));
+        }
+
+        [TestMethod()]
+        public void AddSubjectFalseTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+
+            Assert.IsFalse(sub.AddSubject(aSubject));
+        }
+
+
+        [TestMethod()]
+        public void DeleteSubjectTrueTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+
+            Assert.IsTrue(sub.DeleteSubject(aSubject));
+        }
+
+        [TestMethod()]
+        public void DeleteSubjectFalseTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+
+            Assert.IsFalse(sub.DeleteSubject(aSubject));
+        }
+
+        [TestMethod()]
+        public void UpdateSubjectTrueTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+            aSubject.Name = "BASE DE DATOS";
+            aDatabase.UpdateSubject(aSubject);
+              
+
+            Assert.IsFalse(sub.UpdateSubject(aSubject));
+        }
+
+        [TestMethod()]
+        public void UpdateSubjectFalseTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.UpdateSubject(aSubject);
+                
+
+            Assert.IsFalse(sub.UpdateSubject(aSubject));
+        }
+
+        [TestMethod()]
+        public void GetSubjectTrueTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+
+
+            Assert.IsNotNull(sub.GetSubject(aSubject));
+        }
+
+        [TestMethod()]
+        public void GetSubjectFalseTest()
+        {
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+
+
+            Assert.IsNull(sub.AddSubject(aSubject));
+        }
+
         [TestMethod()]
         public void GetAllSubjectsTest()
         {
-            Assert.Fail();
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+
+
+            Assert.IsNotNull(sub.GetAllSubjects());
         }
 
         [TestMethod()]
         public void GetStudentsBySubjectTest()
         {
-            Assert.Fail();
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+
+
+            Assert.IsNotNull(sub.GetStudentsBySubject(aSubject));
         }
         [TestMethod()]
         public void GetTeachersBySubjectTest()
         {
-            Assert.Fail();
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+
+
+            Assert.IsNotNull(sub.GetTeachersBySubject(aSubject));
         }
 
         [TestMethod()]
         public void GetSubjectsByStudentTest()
         {
-            Assert.Fail();
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+            Student aStudent = new Student();
+
+
+            Assert.IsNotNull(sub.GetSubjectsByStudent(aStudent));
         }
 
         [TestMethod()]
         public void GetSubejctsByTeacherTest()
         {
-            Assert.Fail();
+            SubjectLogic sub = new SubjectLogic(aDatabase);
+            Subject aSubject = new Subject();
+            aDatabase.AddSubject(aSubject);
+            Teacher aTeacher = new Teacher();
+
+            Assert.IsNotNull(sub.GetSubejctsByTeacher(aTeacher));
         }
     }
 }
